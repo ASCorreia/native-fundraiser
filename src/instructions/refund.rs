08895ff35a4428ce65b9402251dbad77b377db13
L0STE <125566964+L0STE@users.noreply.github.com>
2024-11-17 17:12:58 +0100

    fix

--- src/instructions/refund.rs
+++ src/instructions/refund.rs
@@ -1,12 +1,8 @@
 use pinocchio::{
-<<<<<<< Updated upstream
     account_info::AccountInfo,
     instruction::{Seed, Signer},
     sysvars::{clock::Clock, Sysvar},
     ProgramResult, msg,
-=======
-    account_info::AccountInfo, instruction::{Seed, Signer}, sysvars::{clock::Clock, Sysvar}, ProgramResult
->>>>>>> Stashed changes
 };
 
 use pinocchio_token::{state::TokenAccount, instructions::Transfer};
@@ -28,37 +24,14 @@ pub fn refund(accounts: &[AccountInfo], bump: [u8; 1]) -> ProgramResult {
     // let current_time = Clock::get()?.unix_timestamp;
     // assert!(fundraiser_account.time_ending() >= current_time);
 
-<<<<<<< Updated upstream
-    // // Make sure that we didn0t reach the goal
-    let vault_account = unsafe { TokenAccount::from_account_info_unchecked_unsafe(vault) };
-    assert!(fundraiser_account.amount_to_raise() > vault_account.amount());
-    assert_eq!(fundraiser_account.mint_to_raise(), vault_account.mint());
-=======
     // // // Make sure that we didn0t reach the goal
     // let vault_account = unsafe { TokenAccount::from_account_info_unchecked(vault)? } ;
     // assert!(fundraiser_account.amount_to_raise() > vault_account.amount());
     // assert_eq!(&fundraiser_account.mint_to_raise(), vault_account.mint());
->>>>>>> Stashed changes
 
     // let seeds = [Seed::from(fundraiser.key().as_ref()), Seed::from(&bump)];
     // let signer = [Signer::from(&seeds)];
 
-<<<<<<< Updated upstream
-    Transfer {
-        from: vault,
-        to: contributor_ta,
-        authority: vault,
-        amount: Contributor::from_account_info(contributor_account).amount(),
-    }
-    .invoke_signed(&signer)?;
-
-    unsafe {
-        let lamports = contributor_account.borrow_lamports_unchecked();
-        *(contributor_account.borrow_mut_lamports_unchecked()) -= lamports;
-        *(contributor.borrow_mut_lamports_unchecked()) += lamports;
-
-        // contributor.realloc(0, true)?;
-=======
     // Transfer {
     //     from: vault,
     //     to: contributor_ta,
@@ -79,7 +52,6 @@ pub fn refund(accounts: &[AccountInfo], bump: [u8; 1]) -> ProgramResult {
         *(data_ptr.sub(48) as *mut u64) = 0u64;
 
         // contributor_account.close();
->>>>>>> Stashed changes
     }
 
     Ok(())
